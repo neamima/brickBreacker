c5589887871fb0796659884e322dfac9923c8d41
neami <vboxuser@ubun>
2025-07-17 22:21:43 +0000

    Gameplay commit

--- src/main/java/com/mycompany/brickBreacker/Main.java
+++ src/main/java/com/mycompany/brickBreacker/Main.java
@@ -14,11 +14,12 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame obj = new JFrame();
+        Gameplay gamePlay = new Gameplay();
         obj.setBounds(10, 10, 700, 600);
         obj.setTitle("Breackout Ball");
         obj.setResizable(false);
         obj.setVisible(true);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+        obj.add(gamePlay);
     }
 }
