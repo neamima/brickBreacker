3011ee65039f5f25f44f04d231d1a6145645abd8
neami <>
2025-07-17 12:36:45 +0000

    Initial commit

---
+++ src/main/java/com/mycompany/brickBreacker/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.brickBreacker;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author vboxuser
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Breackout Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+}
